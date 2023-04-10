d851db368afe7474530f8f6a930b49713932ea85
alfandi <fandidixpc1@gmail.com>
2023-04-10 15:18:01 +0700

    menambah perintah untuk memanggil procedure pada class matakuliah

--- class_opp_152/class_opp_152.cpp
+++ class_opp_152/class_opp_152.cpp
@@ -40,13 +40,14 @@ public:
         Mahasiswa mhs;
         Matakuliah mk;
 
-        cout << "Nim mahasiswa ";
+        cout << "Nim mahasiswa = ";
         cin >> mhs.nim;
-        cout << "Nama mahasiswa ";
+        cout << "Nama mahasiswa + ";
         cin >> mhs.nama;
 
         mk.InputMK();
         mhs.tampil() ;
         mk.tampilMK();
+
     }
 
